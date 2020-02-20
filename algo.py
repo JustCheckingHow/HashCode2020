e47e2c8f464f01b2097dca297372feeba6acb474
Witold Wydmański <wwydmanski@gmail.com>
2020-02-20 19:43:03 +0100

    feat: naive

--- algo.py
+++ algo.py
@@ -7,12 +7,21 @@ import numpy as np
 
 class NaiveAlgo:
     def __init__(self, libs, book_vals):
-        self.libraries = libs
+        self.libraries = np.array(libs)
         self.book_vals = book_vals
+
+    def get_books_priority(self, library):
+        mapped = [self.book_vals[i] for i in library.books]
+        return np.argsort(mapped)
 
     def solve(self):
         efficiency = [i.get_efficiency(self.book_vals) for i in self.libraries]
-        return np.argsort(efficiency)
+        efficiency = np.argsort(efficiency)[::-1]
+        result_dict = {}
+        for i, lib in zip(efficiency, self.libraries[efficiency]):
+            result_dict[i] = self.get_books_priority(lib)[::-1]
+
+        return result_dict, efficiency
 
 if __name__=="__main__":
     libs, books_values, days = parse_data("data/a_example.txt")
