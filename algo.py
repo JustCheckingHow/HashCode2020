a63708fb32fd7c5c49282243bb5e6dd5f2a9d1e3
pawelkulig1 <pawelkulig1@gmail.com>
2020-02-20 20:39:09 +0100

    left books

--- algo.py
+++ algo.py
@@ -6,9 +6,12 @@ import numpy as np
 
 
 class NaiveAlgo:
-    def __init__(self, libs, book_vals):
+    def __init__(self, libs, book_vals, days):
         self.libraries = np.array(libs)
         self.book_vals = book_vals
+        self.processed = {}
+        self.all_days = days
+        self.day = 0
 
     def get_books_priority(self, library):
         mapped = [self.book_vals[i] for i in library.books]
@@ -20,8 +23,17 @@ class NaiveAlgo:
         result_dict = {}
         for i, lib in zip(efficiency, self.libraries[efficiency]):
             result_dict[i] = self.get_books_priority(lib)[::-1]
+            self.day += lib.signup_time
 
         return result_dict, efficiency
+
+    def get_parsable_books(self, lib):
+        new_books = set(lib.books) - self.processed
+        self.processed += lib.books
+
+        num_books_parsable = (self.all_days - self.day) * lib.number_of_scans
+        return list(new_books)[:num_books_parsable]
+
 
 if __name__=="__main__":
     libs, books_values, days = parse_data("data/a_example.txt")
