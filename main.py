4b77cc034a45f5208731b81288e43bc1056993be
ConsoleRunner <wojciechly@wp.pl>
2020-02-20 21:12:36 +0100

    asd

--- main.py
+++ main.py
@@ -5,18 +5,17 @@ def save_output(filename, libs):
     f = open(filename, 'w')
     
     f.write(str(len(libs)) + '\n')
-    
+    assert len(list(set(libs))) == len(libs)
+    print(len(libs))
     for ID in libs:     
         f.write(f"{ID} {len(libs[ID])}\n")
-
-        for book_id in libs[ID]:
-            f.write(str(book_id) + ' ')
-        f.write('\n')
+        out_str = ' '.join([str(l) for l in libs[ID].tolist()])
+        f.write(out_str + '\n')
 
     f.close()
 
 if __name__ == "__main__":
-    libs, books_values, days = parse_data("data/a_example.txt")
+    libs, books_values, days = parse_data("data/e_so_many_books.txt")
     algo = NaiveAlgo(libs, books_values, days)
     libs, _ = algo.solve()
-    save_output("a_output.txt", libs)
+    save_output("e_output.txt", libs)
