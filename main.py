f26bc60edc848a520befb1d121fc7d8a80b089e9
Witold Wydmański <wwydmanski@gmail.com>
2020-02-20 20:06:55 +0100

    eff

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
+from algo import NaiveAlgo
+from parser_books import parse_data
+
 def save_output(filename, libs):
     f = open(filename, 'a')
     
     f.write(str(len(libs)) + '\n')
     
     for ID in libs:     
-        f.write(str(ID) + ' ' + str(len(libs[ID])) + '\n')
+        f.write(f"{ID} {len(libs[ID])}\n")
 
-        for book_id in libs[str(ID)]:
+        for book_id in libs[ID]:
             f.write(str(book_id) + ' ')
         f.write('\n')
 
     f.close()
+
+if __name__ == "__main__":
+    libs, books_values, days = parse_data("data/a_example.txt")
+    algo = NaiveAlgo(libs, books_values)
+    libs, _ = algo.solve()
+    save_output("a_output.txt", libs)
